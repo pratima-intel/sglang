49e9520199456864bd3ef60b6f141976768e2d14
ZailiWang <zaili.wang@intel.com>
2025-12-17 09:56:51 +0800

    intf fix

--- python/sglang/srt/layers/layernorm.py
+++ python/sglang/srt/layers/layernorm.py
@@ -363,11 +363,11 @@ class LayerNorm(MultiPlatformOp):
         if _is_cpu_amx_available:
             if self.use_bias:
                 return torch.ops.sgl_kernel.layernorm_cpu(
-                    x, self.weight.data, self.bias, self.variance_epsilon
+                    x, self.weight.data, self.bias.data, self.variance_epsilon
                 )
             else:
                 return torch.ops.sgl_kernel.layernorm_cpu(
-                    x, self.weight.data, self.variance_epsilon
+                    x, self.weight.data, None, self.variance_epsilon
                 )
         else:
             return self.forward_native(x)
