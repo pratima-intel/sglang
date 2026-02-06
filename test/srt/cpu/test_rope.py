7a9c1285d1c46c0e51dcbfaae856ee1dca74df74
blzheng <beilei.zheng@intel.com>
2026-02-06 09:57:51 +0800

    rope: add support for bf16 sincos (#102)

--- test/srt/cpu/test_rope.py
+++ test/srt/cpu/test_rope.py
@@ -258,17 +258,21 @@ class TestROPE(CustomTestCase):
         num_heads = 8
         head_size = 72
         qkv = torch.randn(num_tokens, num_heads * head_size * 3).to(torch.bfloat16)
-        query, key, _ = qkv.split([num_heads * head_size, num_heads * head_size, num_heads * head_size], dim=-1)
+        query, key, _ = qkv.split(
+            [num_heads * head_size, num_heads * head_size, num_heads * head_size],
+            dim=-1,
+        )
         query = query.view(num_tokens, num_heads, head_size)
         key = key.view(num_tokens, num_heads, head_size)
-        cos = torch.rand(num_tokens, head_size).to(torch.float32)
-        sin = torch.rand(num_tokens, head_size).to(torch.float32)
-        q_out_ref, k_out_ref = apply_rotary_pos_emb_native(query, key, cos, sin)
-        q_out_sgl, k_out_sgl = torch.ops.sgl_kernel.apply_rotary_pos_emb_cpu(
-            query, key, cos, sin
-        )
-        torch.testing.assert_close(q_out_ref, q_out_sgl, atol=1e-2, rtol=1e-2)
-        torch.testing.assert_close(k_out_ref, k_out_sgl, atol=1e-2, rtol=1e-2)
+        for sincos_dtype in [torch.float32, torch.bfloat16]:
+            cos = torch.rand(num_tokens, head_size).to(sincos_dtype)
+            sin = torch.rand(num_tokens, head_size).to(sincos_dtype)
+            q_out_ref, k_out_ref = apply_rotary_pos_emb_native(query, key, cos, sin)
+            q_out_sgl, k_out_sgl = torch.ops.sgl_kernel.apply_rotary_pos_emb_cpu(
+                query, key, cos, sin
+            )
+            torch.testing.assert_close(q_out_ref, q_out_sgl, atol=1e-2, rtol=1e-2)
+            torch.testing.assert_close(k_out_ref, k_out_sgl, atol=1e-2, rtol=1e-2)
 
 
 if __name__ == "__main__":
