8ecb14d418bcc5be3630d0c7b8458fe0a9cc01dc
blzheng <beilei.zheng@intel.com>
2026-02-28 15:11:10 +0800

    [PR19484] Add logging info for config updates (#104)
    * refactor and format code
    
    * add log for config setting and updating
    
    * only log config update info on rank 0
    
    * refactor code
    
    change log info to debug

--- python/sglang/srt/configs/update_config.py
+++ python/sglang/srt/configs/update_config.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 
+import logging
 from typing import TYPE_CHECKING
 
+from sglang.srt.utils import (
+    log_debug_on_rank0,
+)
+
+logger = logging.getLogger(__name__)
 DEFAULT_MOE_PADDING_SIZE = 32
 
 
@@ -77,19 +83,33 @@ def adjust_tp_num_heads_if_necessary(model_config, tp_size, is_post_update):
                 // model_config.linear_num_key_heads
             )
             if is_post_update:
-                model_config.linear_num_key_heads_cpu = linear_num_key_heads_cpu
-                model_config.linear_num_value_heads_cpu = linear_num_value_heads_cpu
+                update_config(
+                    model_config, "linear_num_key_heads_cpu", linear_num_key_heads_cpu
+                )
+                update_config(
+                    model_config,
+                    "linear_num_value_heads_cpu",
+                    linear_num_value_heads_cpu,
+                )
             else:
-                model_config.linear_num_key_heads = linear_num_key_heads_cpu
-                model_config.linear_num_value_heads = linear_num_value_heads_cpu
+                update_config(
+                    model_config, "linear_num_key_heads", linear_num_key_heads_cpu
+                )
+                update_config(
+                    model_config, "linear_num_value_heads", linear_num_value_heads_cpu
+                )
 
         else:
             if is_post_update:
-                model_config.linear_num_key_heads_cpu = (
-                    model_config.linear_num_key_heads
+                update_config(
+                    model_config,
+                    "linear_num_key_heads_cpu",
+                    model_config.linear_num_key_heads,
                 )
-                model_config.linear_num_value_heads_cpu = (
-                    model_config.linear_num_value_heads
+                update_config(
+                    model_config,
+                    "linear_num_value_heads_cpu",
+                    model_config.linear_num_value_heads,
                 )
 
 
@@ -113,15 +133,29 @@ def update_intermediate_size(model_config, attr_name, intermediate_padding_size)
 
         attr_value = pad_vocab_size(attr_value, intermediate_padding_size)
         if hasattr(model_config, "hf_config"):
-            setattr(model_config.hf_config, attr_name, attr_value)
+            update_config(model_config.hf_config, attr_name, attr_value)
             if hasattr(model_config, "hf_text_config"):
-                setattr(model_config.hf_text_config, attr_name, attr_value)
+                update_config(model_config.hf_text_config, attr_name, attr_value)
             if hasattr(model_config.hf_config, "text_config"):
-                setattr(model_config.hf_config.text_config, attr_name, attr_value)
+                update_config(model_config.hf_config.text_config, attr_name, attr_value)
         else:
-            setattr(model_config, attr_name, attr_value)
+            update_config(model_config, attr_name, attr_value)
 
     return model_config
+
+
+def update_config(model_config, attr_name, new_value):
+    config_name = model_config.__class__.__name__
+    if hasattr(model_config, attr_name):
+        old_value = getattr(model_config, attr_name)
+        if old_value != new_value:
+            log_debug_on_rank0(
+                logger,
+                f"Updating {config_name}.{attr_name} from {old_value} to {new_value}",
+            )
+    else:
+        log_debug_on_rank0(logger, f"Setting {config_name}.{attr_name} to {new_value}")
+    setattr(model_config, attr_name, new_value)
 
 
 def adjust_config_with_unaligned_cpu_tp(
@@ -130,19 +164,17 @@ def adjust_config_with_unaligned_cpu_tp(
     # Support the case where the num_attention_heads is not divisible by the TP size.
     weight_block_size = may_get_weight_block_size(model_config, load_config)
 
-    model_config.hf_config.original_num_attention_heads = (
-        model_config.num_attention_heads
-    )
-    model_config.hf_text_config.original_num_attention_heads = (
-        model_config.num_attention_heads
-    )
-
-    model_config.hf_config.original_total_num_kv_heads = (
-        model_config.get_total_num_kv_heads()
-    )
-    model_config.hf_text_config.original_total_num_kv_heads = (
-        model_config.get_total_num_kv_heads()
-    )
+    for config in [model_config.hf_config, model_config.hf_text_config]:
+        update_config(
+            config,
+            "original_num_attention_heads",
+            model_config.num_attention_heads,
+        )
+        update_config(
+            config,
+            "original_total_num_kv_heads",
+            model_config.get_total_num_kv_heads(),
+        )
 
     if (
         model_config.num_attention_heads % tp_size != 0
@@ -152,9 +184,11 @@ def adjust_config_with_unaligned_cpu_tp(
         if hasattr(model_config.hf_config, "qk_nope_head_dim") and hasattr(
             model_config.hf_config, "qk_rope_head_dim"
         ):
-            model_config.hf_config.qk_head_dim = (
+            update_config(
+                model_config.hf_config,
+                "qk_head_dim",
                 model_config.hf_config.qk_nope_head_dim
-                + model_config.hf_config.qk_rope_head_dim
+                + model_config.hf_config.qk_rope_head_dim,
             )
 
         query_heads_per_kv = (
@@ -163,26 +197,25 @@ def adjust_config_with_unaligned_cpu_tp(
         total_kv_heads = model_config.get_total_num_kv_heads()
         from sglang.srt.layers.vocab_parallel_embedding import pad_vocab_size
 
-        head_dim = (
-                model_config.hidden_size // model_config.num_attention_heads
-            )
+        head_dim = model_config.hidden_size // model_config.num_attention_heads
         if hasattr(model_config.hf_config, "qk_head_dim"):
-            head_dim =  model_config.hf_config.qk_head_dim
+            head_dim = model_config.hf_config.qk_head_dim
         elif hasattr(model_config.hf_text_config, "head_dim"):
-            head_dim =  model_config.hf_text_config.head_dim
+            head_dim = model_config.hf_text_config.head_dim
         elif hasattr(model_config.hf_config, "head_dim"):
-            head_dim =  model_config.hf_text_config.head_dim
+            head_dim = model_config.hf_config.head_dim
 
         pad_size = get_num_heads_padding_size(tp_size, weight_block_size, head_dim)
         num_key_value_heads = pad_vocab_size(total_kv_heads, pad_size)
-        model_config.num_key_value_heads = num_key_value_heads
-        model_config.hf_config.num_key_value_heads = num_key_value_heads
-        model_config.hf_text_config.num_key_value_heads = num_key_value_heads
 
         num_attention_heads = num_key_value_heads * query_heads_per_kv
-        model_config.num_attention_heads = num_attention_heads
-        model_config.hf_config.num_attention_heads = num_attention_heads
-        model_config.hf_text_config.num_attention_heads = num_attention_heads
+        for config in [
+            model_config,
+            model_config.hf_config,
+            model_config.hf_text_config,
+        ]:
+            update_config(config, "num_key_value_heads", num_key_value_heads)
+            update_config(config, "num_attention_heads", num_attention_heads)
 
     adjust_tp_num_heads_if_necessary(model_config.hf_config, tp_size, True)
     if hasattr(model_config.hf_config, "text_config"):
@@ -191,18 +224,15 @@ def adjust_config_with_unaligned_cpu_tp(
         )
 
     intermediate_padding_size = tp_size * get_moe_padding_size(weight_block_size)
-    model_config = update_intermediate_size(
-        model_config, "moe_intermediate_size", intermediate_padding_size
-    )
-    model_config = update_intermediate_size(
-        model_config, "intermediate_size", intermediate_padding_size
-    )
-    model_config = update_intermediate_size(
-        model_config, "intermediate_size_mlp", intermediate_padding_size
-    )
-    model_config = update_intermediate_size(
-        model_config, "shared_expert_intermediate_size", intermediate_padding_size
-    )
+    for moe_intermediate_attr in [
+        "moe_intermediate_size",
+        "intermediate_size",
+        "intermediate_size_mlp",
+        "shared_expert_intermediate_size",
+    ]:
+        model_config = update_intermediate_size(
+            model_config, moe_intermediate_attr, intermediate_padding_size
+        )
 
     multimodal_config = [
         [
@@ -240,10 +270,8 @@ def adjust_config_with_unaligned_cpu_tp(
             and getattr(m_config, config_name).model_type == model_type
         ):
             num_heads = getattr(getattr(m_config, config_name), num_head_str)
-            setattr(
-                getattr(m_config, config_name),
-                "original_" + num_head_str,
-                num_heads,
+            update_config(
+                getattr(m_config, config_name), "original_" + num_head_str, num_heads
             )
             if num_heads % tp_size != 0:
                 from sglang.srt.layers.vocab_parallel_embedding import pad_vocab_size
@@ -252,10 +280,8 @@ def adjust_config_with_unaligned_cpu_tp(
                     tp_size, weight_block_size, head_dim
                 )
                 new_num_heads = pad_vocab_size(num_heads, pad_size)
-                setattr(
-                    getattr(m_config, config_name),
-                    num_head_str,
-                    new_num_heads,
+                update_config(
+                    getattr(m_config, config_name), num_head_str, new_num_heads
                 )
             setattr(
                 m_config,
