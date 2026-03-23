38a2268e154b490499d1f40d9ee6e56f79c31462
jianan-gu <jianan.gu@intel.com>
2026-03-22 23:08:03 -0400

    minor fix after rebase

--- python/sglang/multimodal_gen/runtime/layers/layernorm.py
+++ python/sglang/multimodal_gen/runtime/layers/layernorm.py
@@ -38,16 +38,6 @@ if _is_musa:
     from sgl_kernel import fused_add_rmsnorm
 if not _is_cpu:
     from sglang.jit_kernel.diffusion.triton.norm import norm_infer, rms_norm_fn
-from sglang.jit_kernel.diffusion.triton.rmsnorm_onepass import triton_one_pass_rms_norm
-from sglang.jit_kernel.diffusion.triton.scale_shift import fuse_scale_shift_kernel
-from sglang.jit_kernel.norm import can_use_fused_inplace_qknorm, fused_inplace_qknorm
-from sglang.multimodal_gen.runtime.distributed.parallel_state import (
-    get_tensor_model_parallel_rank,
-    get_tensor_model_parallel_world_size,
-    get_tp_group,
-)
-from sglang.multimodal_gen.runtime.layers.custom_op import CustomOp
-from sglang.multimodal_gen.runtime.utils.common import get_bool_env_var
 
 
 # Copied and adapted from sglang
